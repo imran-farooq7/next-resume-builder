6da2bab05eb50e3477a8c14f0453452b4ec7668b
Imran Farooq <imran_farooq7@hotmail.com>
2024-08-22 19:22:25 +0500

    profile page completed

--- app/(user)/profile/page.tsx
+++ app/(user)/profile/page.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const ProfilePage = () => {
-	return <div>Profile</div>;
+	return (
+		<div className="mt-6">
+			<h1 className=" text-3xl font-bold text-blue-800">Profile</h1>
+			<Tabs defaultValue="basic" className="w-[700px] mt-5">
+				<TabsList>
+					<TabsTrigger value="basic">Basic</TabsTrigger>
+					<TabsTrigger value="education">Education</TabsTrigger>
+					<TabsTrigger value="experience">Experience</TabsTrigger>
+					<TabsTrigger value="skills">Skills</TabsTrigger>
+				</TabsList>
+				<TabsContent value="basic">
+					Make changes to your account here.
+				</TabsContent>
+				<TabsContent value="education">Change your password here.</TabsContent>
+				<TabsContent value="experience">Change your password here.</TabsContent>
+				<TabsContent value="skills">Change your password here.</TabsContent>
+			</Tabs>
+		</div>
+	);
 };
 
 export default ProfilePage;
