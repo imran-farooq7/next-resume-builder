c9555cd7db26d854ebeaff983a224d209a53017e
Imran Farooq <imran_farooq7@hotmail.com>
2024-08-27 04:22:26 +0500

    Template actions added

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 import { auth } from "@/auth";
 import { prisma } from "@/prisma/prisma";
+import { Template } from "@prisma/client";
 import { redirect } from "next/navigation";
 
 export const updateUser = async (resumeProfile: any) => {
@@ -17,13 +18,64 @@ export const updateUser = async (resumeProfile: any) => {
 				resumeProfile: resumeProfile,
 			},
 		});
-		throw new Error("User not found");
 		if (updatedUser) {
 			return {
 				status: "success",
 				message: "Profile updated successfully",
 			};
 		}
+	} catch (error) {
+		return {
+			status: "error",
+			message: "something went wrong",
+		};
+	}
+};
+export const createTemplate = async (template: Template) => {
+	try {
+		const createdTemplate = await prisma.template.create({
+			data: {
+				name: template.name,
+				html: template.html,
+				isPaid: template.isPaid,
+				thumbnail: template.thumbnail,
+			},
+		});
+		if (createdTemplate) {
+			return {
+				status: "success",
+				message: "Template created successfully",
+			};
+		}
+	} catch (error) {
+		return {
+			status: "error",
+			message: "something went wrong",
+		};
+	}
+};
+export const getTemplates = async () => {
+	try {
+		const templates = await prisma.template.findMany();
+		return templates;
+	} catch (error) {
+		return {
+			status: "error",
+			message: "something went wrong",
+		};
+	}
+};
+export const deleteTemplate = async (id: string) => {
+	try {
+		const deletedTemplate = await prisma.template.delete({
+			where: {
+				id: id,
+			},
+		});
+		return {
+			status: "success",
+			message: "Template deleted successfully",
+		};
 	} catch (error) {
 		return {
 			status: "error",
